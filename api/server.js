ad835906b155693d507b65bb8b0f19e6f6cb3bc1
afrizal-10 <adijuliyantoafrizal10@gmail.com>
2025-09-06 14:00:44 +0700

    fix bug api

--- api/server.js
+++ api/server.js
@@ -11,26 +11,15 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: "10mb"}));
 
-// Import routes
-const authRoutes = require("../routes/authRoutes");
-const taskRoutes = require("../routes/taskRoutes");
-const uploadRoutes = require("../routes/uploadRoutes");
-const blogRoutes = require("../routes/blogRoutes");
-const jadwalRoutes = require("../routes/jadwalRoutes");
-
-// Use routes
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/upload", uploadRoutes);
-app.use("/api/blogs", blogRoutes);
-app.use("/api/jadwal", jadwalRoutes);
+app.use("/api/auth", require("../routes/authRoutes"));
+app.use("/api/tasks", require("../routes/taskRoutes"));
+app.use("/api/upload", require("../routes/uploadRoutes"));
+app.use("/api/blogs", require("../routes/blogRoutes"));
+app.use("/api/jadwal", require("../routes/jadwalRoutes"));
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () =>
-    console.log(`🚀 Server running locally on port ${PORT}`)
-  );
+  app.listen(PORT, () => console.log(`🚀 Local on port ${PORT}`));
 }
 
-module.exports = app;
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
