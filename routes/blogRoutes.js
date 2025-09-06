8890f76f91ca36518d70137bd700ec8a3ef99128
afrizal-10 <adijuliyantoafrizal10@gmail.com>
2025-09-06 18:06:10 +0700

    fix bug blog

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -8,16 +8,16 @@ const {
   deleteBlog,
 } = require("../controllers/blogController");
 const multer = require("multer");
+const {protect} = require("../middleware/authMiddleware");
 
 const storage = multer.memoryStorage();
 const upload = multer({storage});
 
-// POST /api/blogs → multer middleware dipasang di route
-router.post("/", upload.single("imageUrl"), createBlog);
-router.put("/:id", upload.single("imageUrl"), updateBlog);
-
-router.get("/", getBlogs);
-router.get("/:id", getBlogById);
-router.delete("/:id", deleteBlog);
+// Semua route yang membutuhkan user login pakai middleware protect
+router.get("/", protect, getBlogs);
+router.get("/:id", protect, getBlogById);
+router.post("/", protect, upload.single("imageUrl"), createBlog); // protect dulu, baru upload
+router.put("/:id", protect, upload.single("imageUrl"), updateBlog);
+router.delete("/:id", protect, deleteBlog);
 
 module.exports = router;
